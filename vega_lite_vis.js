5b7d5bb05b5884d3f2e8527ab287db9dc22652b8
mmus0011 <89535196+mmus0011@users.noreply.github.com>
2021-10-16 22:21:47 +0500

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,8 @@
+var vg_1 = "linechart.json";
+var vg_2 = "maap.json";
+vegaEmbed("#linechart", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+vegaEmbed("#map1", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+   }).catch(console.error);
